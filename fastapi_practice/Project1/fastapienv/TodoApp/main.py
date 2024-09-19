18fdb1d9cf7ef9ce10bb92156586305e814c7661
melanieprettyman <melanie.prettyman98@gmail.com>
2024-09-19 15:17:10 -0600

    testing

---
+++ fastapi_practice/Project1/fastapienv/TodoApp/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from .models import Base
+from .database import engine
+from .routers import auth, todos, admin, user
+
+app = FastAPI()
+
+@app.get("/healthy")
+def health_check():
+    return {'status': 'Healthy'}
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+
+app.include_router(todos.router)
+
+app.include_router(admin.router)
+
+app.include_router(user.router)
